d851ab49c2faf63ccae8f620fb24913fc208d9c5
Claudio9905 <cloudpost10@gmail.com>
2025-06-26 16:49:43 +0200

    U2-W1-D4

---
+++ js/script.js
@@ -0,0 +1,88 @@
+const post1 = document.querySelector(".post-1");
+const post2 = document.querySelector(".post-2");
+const post3 = document.querySelector(".post-3");
+const post4 = document.querySelector(".post-4");
+const post5 = document.querySelector(".post-5");
+const post6 = document.querySelector(".post-6");
+const post7 = document.querySelector(".post-7");
+const post8 = document.querySelector(".post-8");
+
+post1.addEventListener("mouseenter", () => {
+  const pin = document.getElementById("punteruolo");
+  pin.classList.add("punteruoloUp");
+});
+
+post1.addEventListener("mouseleave", () => {
+  const pin = document.getElementById("punteruolo");
+  pin.classList.remove("punteruoloUp");
+});
+
+post2.addEventListener("mouseenter", () => {
+  const pin = document.getElementById("punteruolo");
+  pin.classList.add("punteruoloUp");
+});
+
+post2.addEventListener("mouseleave", () => {
+  const pin = document.getElementById("punteruolo");
+  pin.classList.remove("punteruoloUp");
+});
+
+post3.addEventListener("mouseenter", () => {
+  const pin = document.getElementById("punteruolo");
+  pin.classList.add("punteruoloUp");
+});
+
+post3.addEventListener("mouseleave", () => {
+  const pin = document.getElementById("punteruolo");
+  pin.classList.remove("punteruoloUp");
+});
+
+post4.addEventListener("mouseenter", () => {
+  const pin = document.getElementById("punteruolo");
+  pin.classList.add("punteruoloUp");
+});
+
+post4.addEventListener("mouseleave", () => {
+  const pin = document.getElementById("punteruolo");
+  pin.classList.remove("punteruoloUp");
+});
+
+post5.addEventListener("mouseenter", () => {
+  const pin = document.getElementById("punteruolo");
+  pin.classList.add("punteruoloUp");
+});
+
+post5.addEventListener("mouseleave", () => {
+  const pin = document.getElementById("punteruolo");
+  pin.classList.remove("punteruoloUp");
+});
+
+post6.addEventListener("mouseenter", () => {
+  const pin = document.getElementById("punteruolo");
+  pin.classList.add("punteruoloUp");
+});
+
+post6.addEventListener("mouseleave", () => {
+  const pin = document.getElementById("punteruolo");
+  pin.classList.remove("punteruoloUp");
+});
+
+post7.addEventListener("mouseenter", () => {
+  const pin = document.getElementById("punteruolo");
+  pin.classList.add("punteruoloUp");
+});
+
+post7.addEventListener("mouseleave", () => {
+  const pin = document.getElementById("punteruolo");
+  pin.classList.remove("punteruoloUp");
+});
+
+post8.addEventListener("mouseenter", () => {
+  const pin = document.getElementById("punteruolo");
+  pin.classList.add("punteruoloUp");
+});
+
+post8.addEventListener("mouseleave", () => {
+  const pin = document.getElementById("punteruolo");
+  pin.classList.remove("punteruoloUp");
+});
